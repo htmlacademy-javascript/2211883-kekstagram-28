ed0494d67d58ee8c28f10168727344a2ebb26d93
Perry1510 <ruchkina1210@gmail.com>
2023-03-25 01:12:38 +0300

    Дз 8 раздела

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 import { getPostList } from './data.js';
+import { renderMiniatures } from './miniatures.js';
+import { renderGallery } from './gallery.js';
 
-getPostList(25);
-
-import { renderMiniatures} from './miniatures.js';
-
-renderMiniatures(getPostList());
+const postList = getPostList(25);
+renderMiniatures(postList);
+renderGallery(postList);
